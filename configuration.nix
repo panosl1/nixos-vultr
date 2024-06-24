364a8129fde2f8e66e43dca78c98467569267d4a
Your Name <panos@panix.eu>
2024-06-24 02:51:15 +0200

    Brewfile for Linux

---
+++ configuration.nix
@@ -0,0 +1,94 @@
+{ modulesPath, config, lib, pkgs, ... }: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+  ];
+
+  # allow unfree packages to be installed
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    firewall.enable = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Zurich";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # UNCOMMENT this to enable docker
+  # virtualisation.docker.enable = true;
+
+  programs.fish.enable = true;
+
+  security.sudo.wheelNeedsPassword = false;
+
+  services = {
+    openssh = {
+      enable = true;
+      settings.PasswordAuthentication = false;
+    };
+
+    # UNCOMMENT this to enable headscale
+    # headscale.enable = true;
+
+    # UNCOMMENT this to enable a prometheus node exporter
+    # prometheus.exporters.node.enable = true;
+    
+    # UNCOMMENT this to enable homeassistant-satellite - it's prob necessary to add more configuration here
+    # homeassistant-satellite.enable = true;
+  };
+
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.curl
+    pkgs.gitMinimal
+
+    # UNCOMMENT the following to install these packages systemwide
+    # pkgs.jq
+    # pkgs.neovim
+    # pkgs.fzf
+  ];
+
+  users.users = {
+
+    root = {
+      # change this to your ssh key
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEd7udiygWP2fPGs+8W8Pkj5vX83Civ8TrmrTRhSyAuQ panos@panix.eu"
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFmhiSGRQ/AvB43ZCKa+HC6kwfs9xzZZc2DDrDnpzCMn7NflhAqVGBzso5EZvmBS+XywdSGXjMNvLBcgvHC5qoY= panagiotis.lampropoulosibm.com@panoss-mbp.ams.nl.ibm.com"
+      ];
+    };
+
+    # UNCOMMENT the following to enable the nixos user
+    # nixos = {
+    #   isNormalUser = true; 
+    #   shell = pkgs.fish; 
+    #   description = "nixos user"; 
+    #   extraGroups = [ 
+    #     "networkmanager" 
+    #     "wheel" 
+    #     "docker"
+    #   ]; 
+    #   openssh.authorizedKeys.keys = [
+    #     "CHANGE"
+    #   ];
+    # };
+
+  };
+
+  system.stateVersion = "23.11";
+}
